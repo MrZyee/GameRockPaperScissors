d5ec83bb7051ff1f4fca2c1d9e421d1cceb81656
Bartek Koceniak <bartlomiej.koceniak@outlook.com>
2023-02-17 18:12:12 +0100

    Add project files.

---
+++ Gameplay.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameRockPaperScissors
+{
+    internal class Gameplay
+    {
+        public Gameplay(string User1, string User2)
+        {
+            int pointUser1 = 0;
+            int pointUser2 = 0;
+
+            if (User1 == "paper" && User2 == "rock" || User1 == "scissors" && User2 == "paper" || User1 == "rock" && User2 == "scissors")
+            {
+                Console.WriteLine("User1 WIN!");
+                pointUser1++;
+            }
+            else if (User2 == "paper" && User1 == "rock" || User2 == "scissors" && User1 == "paper" || User2 == "rock" && User1 == "scissors")
+            {
+                Console.WriteLine("User2 WIN!");
+                pointUser2++;
+            }
+            else
+            {
+                Console.WriteLine("remis!");
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using GameRockPaperScissors;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class GameRockPaperScissors
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the game of rock, paper, scissors!");
+
+            int pointUser1 = 0;
+            int pointUser2 = 0;
+
+            while (pointUser1 != 3 || pointUser2 !=3)
+            {
+                Console.WriteLine("Selection of player number 1: ");
+                string User1 = Console.ReadLine();
+                Console.WriteLine("Selection of player number 2: ");
+                string User2 = Console.ReadLine();
+
+                Gameplay win = new Gameplay(User1, User2);
+            }
+
+            if (pointUser1 > pointUser2)
+            {
+                Console.WriteLine("The winner is User1");
+            }
+            else
+            {
+                Console.WriteLine("The winner is User2");
+            }
+            
+        }
+    }
+}
