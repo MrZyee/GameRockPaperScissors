e6480a1a70d3009a604fa87ef8c390b69871d447
Bartek Koceniak <bartlomiej.koceniak@outlook.com>
2023-02-18 07:28:30 +0100

    Aktualizacja

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using GameRockPaperScissors;
-using System;
+﻿using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -12,14 +11,31 @@ namespace MyApp // Note: actual namespace depends on the project name.
             int pointUser1 = 0;
             int pointUser2 = 0;
 
-            while (pointUser1 != 3 || pointUser2 !=3)
+            while (pointUser1 != 3 && pointUser2 != 3)
             {
                 Console.WriteLine("Selection of player number 1: ");
                 string User1 = Console.ReadLine();
                 Console.WriteLine("Selection of player number 2: ");
                 string User2 = Console.ReadLine();
 
-                Gameplay win = new Gameplay(User1, User2);
+                if (User1 == "paper" && User2 == "rock" || 
+                    User1 == "scissors" && User2 == "paper" || 
+                    User1 == "rock" && User2 == "scissors")
+                {
+                    Console.WriteLine("User1 WIN!");
+                    pointUser1++;
+                }
+                else if (User2 == "paper" && User1 == "rock" || 
+                    User2 == "scissors" && User1 == "paper" || 
+                    User2 == "rock" && User1 == "scissors")
+                {
+                    Console.WriteLine("User2 WIN!");
+                    pointUser2++;
+                }
+                else
+                {
+                    Console.WriteLine("remis!");
+                }
             }
 
             if (pointUser1 > pointUser2)
